00815d958abd5fc2b51f41f4c7866a34d5412008
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-11-01 19:38:14 -0200

    Fix gallery flex style

--- src/components/listings/Gallery/index.js
+++ src/components/listings/Gallery/index.js
@@ -86,6 +86,7 @@ export default class ListingGallery extends PureComponent {
         activeOpacity={0.95}
         disabled={!onPressImage}
         onPress={() => onPressImage(index)}
+        style={{width: '100%', height: '100%'}}
       >
         {React.isValidElement(image) ? (
           image
