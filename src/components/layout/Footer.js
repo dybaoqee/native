cbce07ba3a6451dfb63618c50693b0726db06af9
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-11-08 16:16:11 -0200

    Remove redundant code

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,11 +2,8 @@ import styled from 'styled-components'
 import {themeGet} from 'styled-system'
 import {View} from '@emcasa/ui-native'
 
-import {elevation} from '@/assets/styles'
-
-export default styled(View)`
+export default styled(View).attrs({elevation: 4})`
   border-top-color: ${themeGet('colors.lightGrey')};
   border-top-width: 1px;
   background-color: white;
-  ${elevation(2)};
 `
