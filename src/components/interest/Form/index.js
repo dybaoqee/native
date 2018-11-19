caa4bde629e66840bac079fe53bc53daee9ba57b
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-11-18 23:40:49 -0200

    Refactor interest form fields component

--- src/components/interest/Form/index.js
+++ src/components/interest/Form/index.js
@@ -1,10 +1,9 @@
-import {Component} from 'react'
+import {PureComponent} from 'react'
+import * as Final from 'react-final-form'
 import {View, Text, Dropdown} from '@emcasa/ui-native'
-
-import Form from '@/components/shared/Form'
 import Fields from './Fields'
 
-export default class InterestForm extends Component {
+export default class InterestForm extends PureComponent {
   state = {
     activeType: undefined
   }
@@ -12,28 +11,47 @@ export default class InterestForm extends Component {
   onChangeType = (id) => this.setState({activeType: id})
 
   render() {
-    const {interestTypes, ...props} = this.props
+    const {interestTypes, onSubmit, onChange} = this.props
     const {activeType} = this.state
 
     return (
-      <Form {...props}>
-        <View p="15px">
-          <Text>Escolha a melhor forma para agendar sua visita</Text>
-          <Dropdown
-            height="tall"
-            selectedValue={this.state.activeType}
-            onChange={this.onChangeType}
-            placeholder="Como fazemos?"
-          >
-            {interestTypes.map(({id, name}) => (
-              <Dropdown.Option key={id} value={id}>
-                {name}
-              </Dropdown.Option>
-            ))}
-          </Dropdown>
-          {activeType && <Fields type={activeType} />}
-        </View>
-      </Form>
+      <Final.Form onSubmit={onSubmit}>
+        {() => (
+          <View p="15px">
+            <View mb="25px">
+              <Text fontSize={16} color="grey" textAlign="center">
+                Escolha a melhor forma
+                {'\n'}
+                para agendar sua visita
+              </Text>
+            </View>
+            <Final.Field name="interestType" parse={parseInt}>
+              {({input}) => (
+                <Dropdown
+                  height="tall"
+                  selectedValue={input.value || undefined}
+                  onChange={(value) => {
+                    input.onChange(value)
+                    this.onChangeType(value)
+                  }}
+                  placeholder="Como fazemos?"
+                >
+                  {interestTypes.map(({id, name}) => (
+                    <Dropdown.Option key={id} value={parseInt(id)}>
+                      {name}
+                    </Dropdown.Option>
+                  ))}
+                </Dropdown>
+              )}
+            </Final.Field>
+            <View mt="20px">{activeType && <Fields type={activeType} />}</View>
+            <Final.FormSpy
+              subscription={{values: true, valid: true}}
+              onChange={onChange}
+            />
+          </View>
+        )}
+      </Final.Form>
     )
   }
 }
