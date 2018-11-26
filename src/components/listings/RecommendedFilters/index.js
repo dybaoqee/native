a8b87acbed05be4a05c17c7fc1a1d20fc78c2671
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-11-26 17:11:38 -0200

    Add neighrborhood bar images and styles

--- src/components/listings/RecommendedFilters/index.js
+++ src/components/listings/RecommendedFilters/index.js
@@ -7,28 +7,60 @@ import {
 } from 'react-native'
 import styled from 'styled-components'
 import Carousel from 'react-native-snap-carousel'
-import {Text} from '@emcasa/ui-native'
+import LinearGradient from 'react-native-linear-gradient'
+import {View, Text} from '@emcasa/ui-native'
+
+const IMAGE_PADDING_BOTTOM = 10
+const IMAGE_PADDING_TOP = 5
 
 const Neighborhood = styled(function NeighborhoodCard({
+  style,
+  source,
   label,
   onPress,
   ...props
 }) {
+  const width = parseInt(props.width) - 10
+  const height = parseInt(props.height) - 10
   return (
-    <TouchableOpacity onPress={onPress}>
-      <ImageBackground {...props}>
-        <Text color="pink" fontSize={14}>
-          {label}
-        </Text>
-      </ImageBackground>
+    <TouchableOpacity onPress={onPress} activeOpacity={0.85}>
+      <View
+        style={{borderRadius: 5, width, height}}
+        elevation={5}
+        mt={`${IMAGE_PADDING_TOP}px`}
+        mb={`${IMAGE_PADDING_BOTTOM}px`}
+        bg="pink"
+      >
+        <ImageBackground source={source} style={[style, {width, height}]}>
+          <Text color="white" fontSize={14}>
+            {label}
+          </Text>
+        </ImageBackground>
+      </View>
     </TouchableOpacity>
   )
 })`
   align-items: center;
   justify-content: center;
   padding: 5px;
-  width: ${({width}) => parseInt(width) - 10}px;
-  height: ${({height}) => parseInt(height) - 10}px;
+  border-radius: 5px;
+  overflow: hidden;
+`
+
+const GradientOverlay = styled(LinearGradient).attrs({
+  colors: ['rgba(255,255,255,0)', 'white'],
+  locations: [0.8, 1],
+  start: {x: 0, y: 0},
+  end: {x: 1, y: 0},
+  pointerEvents: 'none'
+})`
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
 `
 
 export default class RecommendedNeighborhoods extends PureComponent {
@@ -37,7 +69,7 @@ export default class RecommendedNeighborhoods extends PureComponent {
   static getDerivedStateFromProps({width}) {
     return {
       itemWidth: PixelRatio.roundToNearestPixel(width * 0.4),
-      itemHeight: PixelRatio.roundToNearestPixel(width * 0.4 * 0.64)
+      itemHeight: PixelRatio.roundToNearestPixel(width * 0.4 * 0.55)
     }
   }
 
@@ -58,18 +90,21 @@ export default class RecommendedNeighborhoods extends PureComponent {
     const {style, width, data} = this.props
     const {itemWidth, itemHeight} = this.state
     return (
-      <Carousel
-        enableMomentum
-        inactiveSlideOpacity={0.8}
-        inactiveSlideScale={1}
-        activeSlideAlignment={'start'}
-        containerCustomStyle={style}
-        sliderWidth={width}
-        renderItem={this.renderItem}
-        itemWidth={itemWidth}
-        itemHeight={itemHeight}
-        data={data}
-      />
+      <View>
+        <GradientOverlay />
+        <Carousel
+          enableMomentum
+          inactiveSlideOpacity={0.8}
+          inactiveSlideScale={1}
+          activeSlideAlignment={'start'}
+          containerCustomStyle={{paddingLeft: 15}}
+          sliderWidth={width}
+          renderItem={this.renderItem}
+          itemWidth={itemWidth}
+          itemHeight={itemHeight + IMAGE_PADDING_TOP + IMAGE_PADDING_BOTTOM}
+          data={data}
+        />
+      </View>
     )
   }
 }
--- src/screens/listings/Feed/ListHeader.js
+++ src/screens/listings/Feed/ListHeader.js
@@ -3,16 +3,17 @@ import {connect} from 'react-redux'
 import {compose} from 'recompose'
 import {View, Text} from '@emcasa/ui-native'
 
-import {neighborhoods} from '@/config/filters'
+import neighborhoods from '@/config/filters/neighborhoods'
 import {updateFilters} from '@/redux/modules/search'
 import {
   hasSearchFilters,
-  getSearchFilters
+  getSearchFilters,
+  getSearchCity
 } from '@/redux/modules/search/selectors'
 import ActiveFilters from '@/components/listings/ActiveFilters'
 import RecommendedFilters from '@/components/listings/RecommendedFilters'
 
-function ListHeader({hasFilters, filters, updateFilters}) {
+function ListHeader({hasFilters, filters, city, updateFilters}) {
   const onChange = (nextValue) => updateFilters({...filters, ...nextValue})
   return (
     <View mb="5px" mt="25px" mr="15px" ml="15px">
@@ -23,7 +24,10 @@ function ListHeader({hasFilters, filters, updateFilters}) {
           <Fragment>
             <Text fontWeight="500">Bairros mais buscados</Text>
             <View mr="-15px" ml="-15px">
-              <RecommendedFilters onChange={onChange} data={neighborhoods} />
+              <RecommendedFilters
+                onChange={onChange}
+                data={neighborhoods[city]}
+              />
             </View>
           </Fragment>
         )}
@@ -39,7 +43,8 @@ export default compose(
   connect(
     (state) => ({
       hasFilters: hasSearchFilters(state),
-      filters: getSearchFilters(state)
+      filters: getSearchFilters(state),
+      city: getSearchCity(state)
     }),
     {updateFilters}
   )
