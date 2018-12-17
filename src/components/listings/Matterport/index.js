b191c0d33638b636014634157bd1899653f8f1f3
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-12-17 12:14:02 -0200

    Render spinner while webview is loading

--- src/components/listings/Matterport/index.js
+++ src/components/listings/Matterport/index.js
@@ -1,10 +1,21 @@
 import qs from 'qs'
 import {PureComponent} from 'react'
-import {WebView, View, Platform} from 'react-native'
+import {WebView, View} from 'react-native'
+import {Row} from '@emcasa/ui-native'
 
-import styles from './styles'
+import styled from 'styled-components/native'
+import Spinner from '@/components/shared/Spinner'
 
-const hasWebglSupport = Platform.OS === 'ios' || Platform.Version >= 19 // >= Android 4.4 kitkat
+const Container = styled(View)`
+  flex: 1;
+  z-index: 0;
+  position: relative;
+`
+
+const Content = styled(View)`
+  position: absolute;
+  z-index: 100;
+`
 
 export default class Matterport extends PureComponent {
   static defaultProps = {
@@ -14,29 +25,47 @@ export default class Matterport extends PureComponent {
     useWebKit: true
   }
 
+  state = {loading: true}
+
+  get display() {
+    return {width: this.props.width, height: this.props.height}
+  }
+
+  onLoadEnd = () => this.setState({loading: false}, this.props.onLoadEnd)
+
+  onShouldStartLoadWithRequest = () => this.state.loading
+
+  renderLoading = () => {
+    return (
+      <Row flex={1} bg="dark" justifyContent="center" alignItems="center">
+        <Spinner size={40} color="white" />
+      </Row>
+    )
+  }
+
   render() {
-    const {children, q, code, width, height, ...props} = this.props
+    const {q, code, ...props} = this.props
     const queryString = qs.stringify({
       ...q,
       m: code
     })
     let uri = `https://my.matterport.com/show?${queryString}`
-    const display = {width, height}
     return (
-      <View style={[styles.container, display]}>
-        {code &&
-          hasWebglSupport && (
-            <View style={[styles.content, display]}>
-              <WebView
-                javaScriptEnabled
-                source={{uri}}
-                style={[styles.webview, display]}
-                {...props}
-              />
-            </View>
-          )}
-        <View style={[styles.fallback, display]}>{children}</View>
-      </View>
+      <Container style={this.display}>
+        <Content style={this.display}>
+          <WebView
+            startInLoadingState
+            javaScriptEnabled
+            scalesPageToFit
+            source={{uri}}
+            style={this.display}
+            renderLoading={this.renderLoading}
+            onShouldStartLoadWithRequest={this.onShouldStartLoadWithRequest}
+            onLoadEnd={this.onLoadEnd}
+            {...props}
+          />
+        </Content>
+      </Container>
     )
   }
 }
